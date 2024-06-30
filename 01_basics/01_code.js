4cf8b646bdb7a204509b5bed443e21b6c34b53ba
Harshsrivastava1414 <146131195+Harshsrivastava1414@users.noreply.github.com>
2024-06-30 10:29:28 +0000

    container config done

---
+++ 01_basics/01_code.js
@@ -0,0 +1,15 @@
+const accountId = 11234
+let accountName = "Harsh Sri"
+var accountPassword = "127272"
+accountCity = "Sultanpur"
+let accountState;
+// accountId = 22222   const cannot be change that's why we don't use this
+// let is used for declaring the the variable 
+// var is not used because of issue in block scope and functional scope
+ 
+accountName = "samarth"
+accountPassword = "444443"
+accountCity = " Noida"
+
+
+console.table([ accountId,accountName,accountPassword,accountCity,accountState]);
