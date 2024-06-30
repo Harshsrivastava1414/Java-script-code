ae6fe6abb3eb9dc7d358586f586e1894965b1c66
Harshsrivastava1414 <146131195+Harshsrivastava1414@users.noreply.github.com>
2024-06-30 11:01:53 +0000

    datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,10 @@
+// DataTypes:-
+
+let name = "harsh" // it is  a string data type 
+let phone = 93837 // it is a number data type
+let user_login = false // it is boolean datatype
+
+// let's check all the datatype by using "typeof" in console.log
+
+console.log(typeof phone);
+// O/P is Number
